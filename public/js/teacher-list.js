cc05d60e053f33865128ba6c4465eabe0f9466eb
fengshukuan <123@qq.com>
2017-09-05 21:37:12 +0800

    完成讲师预览功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,4 +1,4 @@
-define(['jquery','template'],function ($,template) {
+define(['jquery','template','bootstrap'],function ($,template) {
     $.ajax({
         type:'get',
         url:'/api/teacher',
@@ -7,6 +7,25 @@ define(['jquery','template'],function ($,template) {
             //解析数据并渲染页面
             var html = template('teacherTpl',{list:data.result});
             $('#teacherInfo').html(html);
+            //绑定预览点击事件
+            $('.preveiw').click(function () {
+                //通过接口获取数据
+                var tcId = $(this).closest('td').attr('data-tcId');
+                $.ajax({
+                    type:'get',
+                    url:'/api/teacher/view',
+                    data:{'tc_id':tcId},
+                    dataType:'json',
+                    success:function (data) {
+                        //解析数据渲染页面
+                        var html= template('modalTpl',data.result);
+                        $('#modalInfo').html(html);
+                        //显示弹窗；
+                        $('#teacherModal').modal();
+                    }
+                })
+            })
         }
     });
+
 });

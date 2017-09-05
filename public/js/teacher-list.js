e7f387d4e9d63bb7592e2777d093e92b1fb79280
fengshukuan <123@qq.com>
2017-09-05 17:13:59 +0800

    讲师列表功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,3 +1,12 @@
 define(['jquery','template'],function ($,template) {
-    console.log(11111)
+    $.ajax({
+        type:'get',
+        url:'/api/teacher',
+        dataType:'json',
+        success:function (data) {
+            //解析数据并渲染页面
+            var html = template('teacherTpl',{list:data.result});
+            $('#teacherInfo').html(html);
+        }
+    });
 });
